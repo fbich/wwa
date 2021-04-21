cd0c303a04fce0e56e5ffabdd538a023cd215612
fbich <francis.bich@sfr.fr>
2021-04-21 08:38:54 +0200

    Added static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,5 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
